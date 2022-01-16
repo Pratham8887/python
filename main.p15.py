4fb7736260c2f54ccc07b36b305d6a37f10ad1e9
Pratham8887 <97673005+Pratham8887@users.noreply.github.com>
2022-01-16 13:59:02 +0530

    Add files via upload

---
+++ main.p15.py
@@ -0,0 +1,9 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruit = fruits[0]
+print(first_fruit)
+second_fruit = fruits[1]
+print(second_fruit)
+third_fruit = fruits[2]
+print(third_fruit)
+four_fruit = fruits[3]
+print(four_fruit)
