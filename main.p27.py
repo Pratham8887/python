ef07e88a83b0e439c1f104bfd98d2ae0c3b4ef74
Pratham8887 <97673005+Pratham8887@users.noreply.github.com>
2022-01-16 14:15:27 +0530

    Add files via upload

---
+++ main.p27.py
@@ -0,0 +1,8 @@
+countries = ['germany', 'france', 'belgium', 'sweden', 'denmark', 'finland', 'norway', 'iceland', 'estonia']
+gr, fr, bg, sw, *scandic, es = countries
+print(gr)
+print(fr)
+print(bg)
+print(sw)
+print(scandic)
+print(es)
