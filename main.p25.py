d03470503a5f955c1024aab659ba03dc4a9eb601
Pratham8887 <97673005+Pratham8887@users.noreply.github.com>
2022-01-16 14:13:49 +0530

    Add files via upload

---
+++ main.p25.py
@@ -0,0 +1,6 @@
+fruits = ['banana', 'orange', 'mango', 'lemon', 'lime', 'apple']
+first_fruit, second_fruit, third_fruit, *rest = fruits
+print(first_fruit)
+print(second_fruit)
+print(third_fruit)
+print(rest)
