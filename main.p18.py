5bfa166741ed46ef70be9d39c5d858a355b2c21a
Pratham8887 <97673005+Pratham8887@users.noreply.github.com>
2022-01-16 14:01:22 +0530

    Add files via upload

---
+++ main.p18.py
@@ -0,0 +1,11 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits = fruits[0:4]
+print(all_fruits)
+all_fruits = fruits[0:]
+print(all_fruits)
+orange_and_mango = fruits[1:3]
+print(orange_and_mango)
+orange_mango_lemon = fruits[1:]
+print(orange_mango_lemon)
+orange_and_lemon = fruits[::2]
+print(orange_and_lemon)
