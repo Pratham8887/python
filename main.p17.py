6da604dade5fe68ba4a02cbaea0f2c4a938af267
Pratham8887 <97673005+Pratham8887@users.noreply.github.com>
2022-01-16 14:00:38 +0530

    Add files via upload

---
+++ main.p17.py
@@ -0,0 +1,9 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits = fruits[:-4]
+print(all_fruits)
+orange_and_mango = fruits[-3:-1]
+print(orange_and_mango)
+orange_mango_lemon = fruits[-3:]
+print(orange_mango_lemon)
+reverse_fruits = fruits[::-1]
+print(reverse_fruits)
