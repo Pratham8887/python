ee4a520d71e30deaa20f0c97ed9e164fdd0f5832
Pratham8887 <97673005+Pratham8887@users.noreply.github.com>
2022-01-16 13:57:16 +0530

    Add files via upload

---
+++ main.p13.py
@@ -0,0 +1,9 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+third_fruit = fruits[-2]
+four_fruit = fruits[-1]
+print(first_fruit)
+print(second_fruit)
+print(third_fruit)
+print(four_fruit)
